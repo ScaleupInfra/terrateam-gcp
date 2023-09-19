d521fa7ea6b450de489659c9a54de56d8e812516
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-19 18:46:26 +0530

    comments removed

--- 1provider.tf
+++ 1provider.tf
@@ -11,10 +11,6 @@ terraform {
   } */
 }
 
-
-# to set region in the gcp, go to shell and type this command "gcloud config set compute/region us-cental1-a"
-# to set zone in the gcp, go to shell and type this command "gcloud config set compute/zone us-cental1"
-
 provider "google" {
   project     = "terrateam-demo"
   region      = "us-central1"
